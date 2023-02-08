d962755d8cc91e469ee9ae617ae6fdbec67c9c73
Sourav2707 <smartsourav33@gmail.com>
2023-02-08 14:23:32 +0530

    UC1 Ability to create a payroll service database and have java program connect to database

--- src/main/java/com/bridgelabs/day31_employee_payroll_service/Main.java
+++ src/main/java/com/bridgelabs/day31_employee_payroll_service/Main.java
@@ -1,8 +1,16 @@
 package com.bridgelabs.day31_employee_payroll_service;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         System.out.println("Welcome to day 31 employee payroll sercvice using JDBC!");
+        String url = "jdbc:mysql://localhost:3306/Day31EmployeePayRollService";
+        String user = "root";
+        String password = "root";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+//        ResultSet resultSet = statement.executeQuery("");
     }
 }
